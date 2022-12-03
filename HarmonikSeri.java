4ffc648565c5d0f64bb7ef5acb1eceba030d7518
efedemir1 <114537116+efedemir1@users.noreply.github.com>
2022-12-04 01:54:53 +0300

    Add files via upload

---
+++ HarmonikSeri.java
@@ -0,0 +1,14 @@
+package JavaProject;
+import java.util.Scanner;
+public class HarmonikSeri {
+    public static void main(String[] args) {
+        Scanner inp= new Scanner(System.in);
+        System.out.println("N sayısını giriniz");
+        int n= inp.nextInt();
+        double result=0.0;
+        for (int i =1;i<=n; i++) {
+        result += (1.0/i);
+        }
+    System.out.println(result);
+    }
+}
