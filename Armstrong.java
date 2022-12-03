c002bba1b2cb4e478becef480bac04f3b480c2d3
efedemir1 <114537116+efedemir1@users.noreply.github.com>
2022-12-04 01:53:59 +0300

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,33 @@
+package JavaProject;
+import java.util.Scanner;
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Sayı giriniz : ");
+        int number = inp.nextInt();
+        int basNumber = 0;
+        int tempNumber = number;
+        int basValue;
+        int result = 0;
+        int basPow;
+        while (tempNumber != 0) {
+            tempNumber /= 10;
+            basNumber++;
+        }
+        tempNumber = number;
+        while (tempNumber != 0) {
+            basValue = tempNumber % 10;
+            basPow = 1;
+            for (int i = 1; i <= basNumber; i++) {
+                basPow *= basValue;
+            }
+            result += basPow;
+            tempNumber /= 10;
+        }
+        if (result == number) {
+            System.out.println(number +  " Sayısı bir Amstrong sayısıdır");
+        } else {
+            System.out.println(number +  " Sayısı bir Amstrong sayısı değildir");
+        }
+    }
+}
